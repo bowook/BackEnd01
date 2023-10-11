267053e25e955d127cd3e5105d1bf53a0d9dd0e3
bowook <mbosx316@naver.com>
2023-10-11 17:30:30 +0900

    Initial commit

---
+++ src/test/java/com/example/backend01/BackEnd01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backend01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEnd01ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
